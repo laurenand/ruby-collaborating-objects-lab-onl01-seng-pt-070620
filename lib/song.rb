fc25ab7fcb36f984cb1406fe9ab31faf06024eae
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-10 17:07:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,9 @@ class Song
   end
   
   def self.new_by_filename(filename)
+    song = filename.split(" - ")[1]
+    song.artist_name = self
     
-    binding.pry
   end
   
   def artist_name=(name)
